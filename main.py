132bd41440c4844638664348c3f50cb0f1844468
pranayjasrasaria <pranayjasrasaria@gmail.com>
2026-03-24 21:03:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+wrong_number=36
+correct_number=56
+print("Correct number is ",correct_number)
+
+wrong_number=38
+correct_number=48
+print("Correct number is ",correct_number)
+
+
+wrong_number=93
+correct_number=82
+print("Correct number is ",correct_number)
+
+wrong_number=67
+correct_number=76
+print("Correct number is ",correct_number)
